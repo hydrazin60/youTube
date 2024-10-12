28bd2cb1d9a686f3f3a4ced8e385bccca4227cde
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-12 16:43:12 +0545

    user dashbord Ui create

--- frontend/src/page/YoutubeStudioPage.jsx
+++ frontend/src/page/YoutubeStudioPage.jsx
@@ -5,6 +5,18 @@ import { IoFilterSharp } from "react-icons/io5";
 import { FaArrowDown } from "react-icons/fa";
 import { MdOutlineLock } from "react-icons/md";
 
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { MdOutlineAnalytics } from "react-icons/md";
+import { MdOutlineInsertComment } from "react-icons/md";
+import { MdOutlineSubtitles } from "react-icons/md";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
+import { FiDollarSign } from "react-icons/fi";
+import { LiaFileAudio } from "react-icons/lia";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BiCommentError } from "react-icons/bi";
+import { icons } from "lucide-react";
+
 export default function YoutubeStudioPage() {
   const contentList = [
     {
@@ -36,11 +48,99 @@ export default function YoutubeStudioPage() {
       name: "Promotions",
     },
   ];
+  const YourChannelcontrollerIconsList = [
+    {
+      id: 1,
+      name: "Dashboard",
+      icon: <LuLayoutDashboard />,
+    },
+    {
+      id: 2,
+      name: "Content",
+      icon: <MdOutlineVideoLibrary />,
+    },
+    {
+      id: 3,
+      name: "Analytics",
+      icon: <MdOutlineAnalytics />,
+    },
+    {
+      id: 4,
+      name: "Comments",
+      icon: <MdOutlineInsertComment />,
+    },
+    {
+      id: 5,
+      name: "Subtitles",
+      icon: <MdOutlineSubtitles />,
+    },
+    {
+      id: 6,
+      name: "Copyright",
+      icon: <AiOutlineCopyrightCircle />,
+    },
+    {
+      id: 7,
+      name: "Earn",
+      icon: <FiDollarSign />,
+    },
+    {
+      id: 8,
+      name: "AUdio libary",
+      icon: <LiaFileAudio />,
+    },
+    {
+      id: 9,
+      name: "Settings",
+      icon: <IoSettingsOutline />,
+    },
+    {
+      id: 10,
+      name: "Send feedback",
+      icon: <BiCommentError />,
+    },
+  ];
   const { id } = useParams();
   return (
     <main className="h-screen w-screen bg-zinc-800  mt-auto">
       <div className="h-full w-full flex">
-        <div className="h-full w-[16%] bg-zinc-800  border-r border-zinc-600"></div>
+        <div className="h-full w-[16%] py-4 bg-zinc-800  border-r border-zinc-600 flex justify-center flex-col gap-2">
+          <div className="px-12">
+            <span>
+              <img
+                src="https://scontent.fktm21-1.fna.fbcdn.net/v/t39.30808-6/292694884_729606568152974_711651807545817504_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=kZulNBXicMcQ7kNvgHvAYuW&_nc_ht=scontent.fktm21-1.fna&_nc_gid=ATlu7BHpCIHLCeTNkYFrL9H&oh=00_AYC0s7ucvGbiKxPfP0uDe871f2ZhrPIpXoLtS_r5PuetFg&oe=67101964"
+                alt=""
+                className="w-24 h-24 rounded-full"
+              />
+            </span>
+            <span className=" flex items-center flex-col ">
+              <p className=" font-semibold">Your channel</p>
+              <p className="text-sm text-zinc-400">+2Helper</p>
+            </span>
+          </div>
+          <div className="flex flex-col h-full  ">
+            {YourChannelcontrollerIconsList.map((item) => {
+              return (
+                <>
+                  <div
+                    key={item.id}
+                    className="flex items-center gap-4 p-3 hover:bg-black rounded-md pl-4"
+                  >
+                    <>
+                      <span className="text-zinc-100 text-xl">{item.icon}</span>
+                      <span className="text-[0.8rem] font-semibold   text-zinc-400">
+                        {item.name}
+                      </span>
+                    </>
+                  </div>
+                  {item.id === 8 ? (
+                    <div className="h-[1px] w-full bg-zinc-600 my-1"></div>
+                  ) : null}
+                </>
+              );
+            })}
+          </div>
+        </div>
         <div className="h-full w-[85%] bg-zinc-800 flex flex-col">
           <div className="h-[14%] w-full border-b border-zinc-600 flex flex-col gap-5 p-4">
             <div>
@@ -96,10 +196,10 @@ export default function YoutubeStudioPage() {
                     </th>
                   </tr>
                 </thead>
-                <tbody className="border-b border-zinc-600">
+                <tbody className="border-b border-zinc-600 hover:bg-black">
                   <tr>
-                    <th className="text-xs  text-zinc-400 font-medium px-12 py-2">
-                      <div className="pl-8 flex flex-row gap-2 py-2">
+                    <th className="text-xs  text-zinc-400 font-medium px-7 py-2 ">
+                      <div className="pl-8 flex flex-row gap-2 ">
                         <span>
                           <img
                             src="https://scontent.fktm21-1.fna.fbcdn.net/v/t39.30808-6/292694884_729606568152974_711651807545817504_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=kZulNBXicMcQ7kNvgHvAYuW&_nc_ht=scontent.fktm21-1.fna&_nc_gid=ATlu7BHpCIHLCeTNkYFrL9H&oh=00_AYC0s7ucvGbiKxPfP0uDe871f2ZhrPIpXoLtS_r5PuetFg&oe=67101964"
@@ -117,7 +217,7 @@ export default function YoutubeStudioPage() {
                         </span>
                       </div>
                     </th>
-                    <th>
+                    <th className="px-4 py-2">
                       <div className="flex flex-row gap-2 items-center ">
                         <MdOutlineLock className="text-sm text-white" />
                         <p className=" text-[.7rem] text-white  font-normal">
@@ -125,14 +225,14 @@ export default function YoutubeStudioPage() {
                         </p>
                       </div>
                     </th>
-                    <th>
+                    <th className="px-4 py-2">
                       <div>
                         <p className=" text-[.7rem] font-normal text-white">
                           Made for Kids
                         </p>
                       </div>
                     </th>
-                    <th>
+                    <th className="px-2 py-2">
                       <div className="flex flex-col  items-center">
                         <p className=" text-[.7rem] text-white  font-normal">
                           Jan 1, 2022
@@ -142,21 +242,21 @@ export default function YoutubeStudioPage() {
                         </p>
                       </div>
                     </th>
-                    <th className="px-4 py-2">
+                    <th className="px-5 py-2">
                       <div>
                         <p className=" text-[.7rem] text-white  font-normal">
                           100k
                         </p>
                       </div>
                     </th>
-                    <th className="px-4 py-2">
+                    <th className="px-8 py-2">
                       <div>
                         <p className=" text-[.7rem] text-white  font-normal">
                           10k
                         </p>
                       </div>
                     </th>
-                    <th className="px-2 py-2">
+                    <th className=" py-2">
                       <div className="flex flex-col items-center">
                         <p className="text-[.7rem] text-white  font-normal">
                           100k
